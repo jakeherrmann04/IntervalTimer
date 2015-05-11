01ee8cd098e17a978c39e1d69f81d0fe2c342e3e
jakeherrmann04 <jake.herrmann04@gmail.com>
2015-05-11 16:23:28 -0600

    First Commit

---
+++ IntervalTimer/AddTimeViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTimeViewController.h
+//  IntervalTimer
+//
+//  Created by Jake Herrmann on 5/11/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTimeViewController : UIViewController
+
+@end
---
+++ IntervalTimer/IntervalTimerViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntervalTimerViewController.h
+//  IntervalTimer
+//
+//  Created by Jake Herrmann on 4/30/15.
+//  Copyright (c) 2015 Jake Herrmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntervalTimerViewController : UIViewController
+
+@end
